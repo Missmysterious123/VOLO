8ce4b3195eab2473288f1dbf8a4da91d72ea3bf0
Swaliha Mulla <swaliharoonmulla192@gmail.com>
2025-11-04 09:14:47 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5383804744-172a1",
+  "appId": "1:84469599231:web:b6d0510b68d11126ed35ca",
+  "apiKey": "AIzaSyDmC3nKZr3P3mhk1x_GZ41nX58Im4gokp4",
+  "authDomain": "studio-5383804744-172a1.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "84469599231"
+};
